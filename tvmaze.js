90ffe19640247de7c515613e6c0d7b622efb18b5
Matthew Eckes <eckesm@gmail.com>
2021-01-28 21:36:21 -0500

    completed project and started Jasmine testing

--- tvmaze.js
+++ tvmaze.js
@@ -1,90 +1,176 @@
-/** Given a query string, return array of matching shows:
- *     { id, name, summary, episodesUrl }
- */
+/********************************************************************
+--------------------------- searchShows() ---------------------------
+*********************************************************************
 
+ * DESCRIPTION:
+ * given a search criteria, creates an array of show objects based on API responses.     */
 
-/** Search Shows
- *    - given a search term, search for tv shows that
- *      match that query.  The function is async show it
- *       will be returning a promise.
- *
- *   - Returns an array of objects. Each object should include
- *     following show information:
- *    {
-        id: <show id>,
-        name: <show name>,
-        summary: <show summary>,
-        image: <an image from the show data, or a default imege if no image exists, (image isn't needed until later)>
-      }
- */
-async function searchShows(query) {
-  // TODO: Make an ajax request to the searchShows api.  Remove
-  // hard coded data.
+async function searchShows(q) {
+	// send GET request with show seach parameters to API and await show data in response
+	const res = await axios.get('http://api.tvmaze.com/search/shows', { params: { q } });
 
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary: "<p><b>The Bletchley Circle</b> follows the journey of four ordinary women with extraordinary skills that helped to end World War II.</p><p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their normal lives, modestly setting aside the part they played in producing crucial intelligence, which helped the Allies to victory and shortened the war. When Susan discovers a hidden code behind an unsolved murder she is met by skepticism from the police. She quickly realises she can only begin to crack the murders and bring the culprit to justice with her former friends.</p>",
-      image: "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ]
+	// reduce results of API request to an array of objects containing show data.
+	return res.data.reduce(function(showsArr, nextShow) {
+		let imageUrl;
+		nextShow.show.image === null
+			? (imageUrl = 'https://tinyurl.com/tv-missing')
+			: (imageUrl = nextShow.show.image.original);
+
+		showsArr.push({
+			id      : nextShow.show.id,
+			name    : nextShow.show.name,
+			summary : nextShow.show.summary,
+			image   : imageUrl
+    });
+		return showsArr;
+	}, []);
 }
 
+/********************************************************************
+------------------------- populateShows() ---------------------------
+*********************************************************************
 
-
-/** Populate shows list:
- *     - given list of shows, add shows to DOM
- */
+ * DESCRIPTION:
+ * given an array of show objects from searchShows(), populates the DOM with a card for each show object.     */
 
 function populateShows(shows) {
-  const $showsList = $("#shows-list");
-  $showsList.empty();
+	// clear any existing content in shows list div
+	const $showsList = $('#shows-list');
+	$showsList.empty();
 
-  for (let show of shows) {
-    let $item = $(
-      `<div class="col-md-6 col-lg-3 Show" data-show-id="${show.id}">
+	// create new show card for each show object of the array passed from seachShows()
+	for (let show of shows) {
+		const $item = $(
+			`<div class="col-md-6 col-lg-3 mb-5 show" data-show-id="${show.id}">
          <div class="card" data-show-id="${show.id}">
+           <img class="card-img-top" src="${show.image}">
            <div class="card-body">
              <h5 class="card-title">${show.name}</h5>
              <p class="card-text">${show.summary}</p>
            </div>
          </div>
+         <button class="btn btn-info episodebutton">Episode Info</button>
        </div>
-      `);
+      `
+		);
 
-    $showsList.append($item);
-  }
+		// add new show card to shows list div
+		$showsList.append($item);
+	}
 }
 
+/*******************************************************************
+ -------------------------- getEpisodes() ---------------------------
+ ********************************************************************
 
-/** Handle search form submission:
- *    - hide episodes area
- *    - get list of matching shows and show in shows list
- */
-
-$("#search-form").on("submit", async function handleSearch (evt) {
-  evt.preventDefault();
-
-  let query = $("#search-query").val();
-  if (!query) return;
-
-  $("#episodes-area").hide();
-
-  let shows = await searchShows(query);
-
-  populateShows(shows);
-});
-
-
-/** Given a show ID, return list of episodes:
- *      { id, name, season, number }
- */
+ * DESCRIPTION:
+ * given a show id, returns an array of episodes of that show.     */
 
 async function getEpisodes(id) {
-  // TODO: get episodes from tvmaze
-  //       you can get this by making GET request to
-  //       http://api.tvmaze.com/shows/SHOW-ID-HERE/episodes
+	// send GET request with show id parameters to API and await episode data in response
+	const res = await axios.get(`http://api.tvmaze.com/shows/${id}/episodes`);
 
-  // TODO: return array-of-episode-info, as described in docstring above
+	// reduce results of API request to an array of objects containing episode data and pass array to populateEpisodes()
+	populateEpisodes(
+		res.data.reduce(function(episodesArr, nextEpisode) {
+			episodesArr.push({
+				id          : nextEpisode.id,
+				name        : nextEpisode.name,
+				summary     : nextEpisode.summary,
+				season      : nextEpisode.season,
+				number      : nextEpisode.number,
+				episodesUrl : nextEpisode.url
+      });
+			return episodesArr;
+		}, [])
+	);
 }
+
+/********************************************************************
+ ------------------------- populateEpisodes() -----------------------
+ ********************************************************************
+
+ * DESCRIPTION:
+ * --> creates a list of episode information from an array of episode objects.     */
+
+function populateEpisodes(episodes) {
+	// show episodes area
+	const $episodesArea = $('#episodes-area');
+	$episodesArea.show();
+
+	// empty episodes list of any existing information
+	const $episodesList = $('#episodes-list');
+	$episodesList.empty();
+
+	// create a new li for each show object in array
+	for (let episode of episodes) {
+		let itemHtml = `<b>${episode.name}</b>`;
+
+		// build li content based on what information is available in episode object
+		if (episode.season !== null || episode.number !== null) itemHtml += ' (';
+		if (episode.season !== null) itemHtml += `season ${episode.season}`;
+		if (episode.season !== null && episode.number !== null) itemHtml += ', ';
+		if (episode.number !== null) itemHtml += `episode ${episode.number}`;
+		if (episode.season !== null || episode.number !== null) itemHtml += ')';
+		if (episode.summary !== null) itemHtml += `: ${episode.summary}`;
+		const $item = `<li class="episode">${itemHtml}</li>`;
+
+		// append new li to episodes list
+		$episodesList.append($item);
+	}
+}
+
+/********************************************************************
+---------------------------- When DOM Loads -------------------------
+********************************************************************/
+
+/* ADD EVENT LISTENERS TO BUTTONS
+ * hide episodes area, get list of matching shows and show in shows list.     */
+$('#search-form').on('submit', async function handleSearch(evt) {
+	evt.preventDefault();
+
+	let query = $('#search-query').val();
+	if (!query) return;
+
+	$('#episodes-area').hide();
+
+	let shows = await searchShows(query);
+	populateShows(shows);
+
+	// adds event listener to all buttons created at the bottom of show cards
+	$('.episodebutton').click(function(evt) {
+		const showId = evt.target.parentElement.getAttribute('data-show-id');
+		getEpisodes(showId);
+	});
+});
+
+/* RANDOM STARTING SHOW IN SEARCH
+ * enter a different show on reload.     */
+const randomShows = [
+	'CSI',
+	"Queen's Gambit",
+	'Criminal',
+	'Game of Thrones',
+	'Downton Abbey',
+	'Mad Men',
+	'Rick and Morty',
+	"Bob's Burgers",
+	'Haunting of Hill House',
+	'Forensic Files',
+	'Law & Order',
+	'Bones',
+	'Bodyguard',
+	'The Crown',
+	'Pokémon',
+	'X-Files',
+	'Battlestar Galactica',
+	'Evil',
+	'Real Housewives',
+	'Attack on Titan',
+	'Golden Girls'
+];
+function getRandomShow(shows) {
+	randomNum = Math.floor(Math.random() * shows.length);
+	return shows[randomNum];
+}
+$('#search-query').val(getRandomShow(randomShows));
