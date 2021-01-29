52cb1e9fd03b22a30369fe2bbcd7faba112f920c
Matthew Eckes <eckesm@gmail.com>
2021-01-29 11:25:18 -0500

    navbar search, corrected https, default tv image

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+//DEFAULT
+const defaultImage = 'https://tinyurl.com/tv-missing';
+
 /********************************************************************
 --------------------------- searchShows() ---------------------------
 *********************************************************************
@@ -7,21 +10,19 @@
 
 async function searchShows(q) {
 	// send GET request with show seach parameters to API and await show data in response
-	const res = await axios.get('http://api.tvmaze.com/search/shows', { params: { q } });
+	const res = await axios.get('https://api.tvmaze.com/search/shows', { params: { q } });
 
 	// reduce results of API request to an array of objects containing show data.
 	return res.data.reduce(function(showsArr, nextShow) {
 		let imageUrl;
-		nextShow.show.image === null
-			? (imageUrl = 'https://tinyurl.com/tv-missing')
-			: (imageUrl = nextShow.show.image.original);
+		nextShow.show.image === null ? (imageUrl = defaultImage) : (imageUrl = nextShow.show.image.original);
 
 		showsArr.push({
 			id      : nextShow.show.id,
 			name    : nextShow.show.name,
 			summary : nextShow.show.summary,
 			image   : imageUrl
-    });
+		});
 		return showsArr;
 	}, []);
 }
@@ -41,15 +42,15 @@ function populateShows(shows) {
 	// create new show card for each show object of the array passed from seachShows()
 	for (let show of shows) {
 		const $item = $(
-			`<div class="col-md-6 col-lg-3 mb-5 show" data-show-id="${show.id}">
-         <div class="card" data-show-id="${show.id}">
+			`<div class="col-md-6 col-lg-3 mb-5 show" data-show-id="${show.id}" data-show-name="${show.name}">
+         <div class="card" data-show-id="${show.id}" data-show-name="${show.name}">
            <img class="card-img-top" src="${show.image}">
-           <div class="card-body">
+           <div class="card-body border">
              <h5 class="card-title">${show.name}</h5>
              <p class="card-text">${show.summary}</p>
            </div>
-         </div>
-         <button class="btn btn-info episodebutton">Episode Info</button>
+					 <button class="btn btn-info episodebutton">Episode Info</button>
+				 </div>
        </div>
       `
 		);
@@ -66,21 +67,26 @@ function populateShows(shows) {
  * DESCRIPTION:
  * given a show id, returns an array of episodes of that show.     */
 
-async function getEpisodes(id) {
+async function getEpisodes(showId, showName) {
 	// send GET request with show id parameters to API and await episode data in response
-	const res = await axios.get(`http://api.tvmaze.com/shows/${id}/episodes`);
+	const res = await axios.get(`https://api.tvmaze.com/shows/${showId}/episodes`);
 
 	// reduce results of API request to an array of objects containing episode data and pass array to populateEpisodes()
 	populateEpisodes(
 		res.data.reduce(function(episodesArr, nextEpisode) {
+			let imageUrl;
+			nextEpisode.image === null ? (imageUrl = defaultImage) : (imageUrl = nextEpisode.image.original);
+
 			episodesArr.push({
 				id          : nextEpisode.id,
 				name        : nextEpisode.name,
+				show        : showName,
 				summary     : nextEpisode.summary,
 				season      : nextEpisode.season,
 				number      : nextEpisode.number,
-				episodesUrl : nextEpisode.url
-      });
+				episodesUrl : nextEpisode.url,
+				image       : imageUrl
+			});
 			return episodesArr;
 		}, [])
 	);
@@ -95,28 +101,42 @@ async function getEpisodes(id) {
 
 function populateEpisodes(episodes) {
 	// show episodes area
-	const $episodesArea = $('#episodes-area');
-	$episodesArea.show();
+	// const $episodesArea = $('#episodes-area');
+	// $episodesArea.show();
 
 	// empty episodes list of any existing information
 	const $episodesList = $('#episodes-list');
-	$episodesList.empty();
+	// $episodesList.empty();
+
+	// modal
+	const $episodesModalList = $('#episodesModalList');
+	$episodesModalList.empty();
+
+	let $episodesModalTitle = $('#episodesModalTitle');
+	$episodesModalTitle.text(episodes[0].show)
 
 	// create a new li for each show object in array
 	for (let episode of episodes) {
-		let itemHtml = `<b>${episode.name}</b>`;
+		let itemHtml = `<b class="display-5">${episode.name}</b><br>`;
 
 		// build li content based on what information is available in episode object
-		if (episode.season !== null || episode.number !== null) itemHtml += ' (';
+		if (episode.season !== null || episode.number !== null) itemHtml += '<p class="fst-italic">';
 		if (episode.season !== null) itemHtml += `season ${episode.season}`;
 		if (episode.season !== null && episode.number !== null) itemHtml += ', ';
 		if (episode.number !== null) itemHtml += `episode ${episode.number}`;
-		if (episode.season !== null || episode.number !== null) itemHtml += ')';
-		if (episode.summary !== null) itemHtml += `: ${episode.summary}`;
+		if (episode.season !== null || episode.number !== null) itemHtml += '</p>';
+		if (episode.image !== defaultImage)
+			itemHtml += `<img src="${episode.image}" 
+		class="img-thumbnail" alt="..."></img>`;
+		if (episode.summary !== null) itemHtml += `${episode.summary}`;
+
 		const $item = `<li class="episode">${itemHtml}</li>`;
 
 		// append new li to episodes list
-		$episodesList.append($item);
+		// $episodesList.append($item);
+
+		// append new li to episodes modal
+		$episodesModalList.append($item);
 	}
 }
 
@@ -126,10 +146,18 @@ function populateEpisodes(episodes) {
 
 /* ADD EVENT LISTENERS TO BUTTONS
  * hide episodes area, get list of matching shows and show in shows list.     */
-$('#search-form').on('submit', async function handleSearch(evt) {
+$('#search-form').on('submit', function(evt) {
 	evt.preventDefault();
+	searchInput('search-query');
+});
 
-	let query = $('#search-query').val();
+$('#navbarForm').on('submit', function(evt) {
+	evt.preventDefault();
+	searchInput('navbarSearchQuery');
+});
+
+async function searchInput(inputId) {
+	let query = $(`#${inputId}`).val();
 	if (!query) return;
 
 	$('#episodes-area').hide();
@@ -140,9 +168,13 @@ $('#search-form').on('submit', async function handleSearch(evt) {
 	// adds event listener to all buttons created at the bottom of show cards
 	$('.episodebutton').click(function(evt) {
 		const showId = evt.target.parentElement.getAttribute('data-show-id');
-		getEpisodes(showId);
+		const showName = evt.target.parentElement.getAttribute('data-show-name');
+		getEpisodes(showId, showName);
+
+		// show episodes modal
+		$('#episodesModal').modal('show');
 	});
-});
+}
 
 /* RANDOM STARTING SHOW IN SEARCH
  * enter a different show on reload.     */
@@ -167,10 +199,12 @@ const randomShows = [
 	'Evil',
 	'Real Housewives',
 	'Attack on Titan',
-	'Golden Girls'
+	'Golden Girls',
+	'Rachel Maddow'
 ];
 function getRandomShow(shows) {
 	randomNum = Math.floor(Math.random() * shows.length);
 	return shows[randomNum];
 }
 $('#search-query').val(getRandomShow(randomShows));
+$('#navbarSearchQuery').val(getRandomShow(randomShows));
