27def14c37b3391a64e2adaf725a3eca27dc0899
Matthew Eckes <eckesm@gmail.com>
2021-01-29 11:21:02 -0500

    moved search to navbar

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //DEFAULT
-const defaultImage='https://tinyurl.com/tv-missing'
+const defaultImage = 'https://tinyurl.com/tv-missing';
 
 /********************************************************************
 --------------------------- searchShows() ---------------------------
@@ -15,9 +15,7 @@ async function searchShows(q) {
 	// reduce results of API request to an array of objects containing show data.
 	return res.data.reduce(function(showsArr, nextShow) {
 		let imageUrl;
-		nextShow.show.image === null
-			? (imageUrl = defaultImage)
-			: (imageUrl = nextShow.show.image.original);
+		nextShow.show.image === null ? (imageUrl = defaultImage) : (imageUrl = nextShow.show.image.original);
 
 		showsArr.push({
 			id      : nextShow.show.id,
@@ -44,8 +42,8 @@ function populateShows(shows) {
 	// create new show card for each show object of the array passed from seachShows()
 	for (let show of shows) {
 		const $item = $(
-			`<div class="col-md-6 col-lg-3 mb-5 show" data-show-id="${show.id}">
-         <div class="card" data-show-id="${show.id}">
+			`<div class="col-md-6 col-lg-3 mb-5 show" data-show-id="${show.id}" data-show-name="${show.name}">
+         <div class="card" data-show-id="${show.id}" data-show-name="${show.name}">
            <img class="card-img-top" src="${show.image}">
            <div class="card-body border">
              <h5 class="card-title">${show.name}</h5>
@@ -69,21 +67,20 @@ function populateShows(shows) {
  * DESCRIPTION:
  * given a show id, returns an array of episodes of that show.     */
 
-async function getEpisodes(id) {
+async function getEpisodes(showId, showName) {
 	// send GET request with show id parameters to API and await episode data in response
-	const res = await axios.get(`https://api.tvmaze.com/shows/${id}/episodes`);
+	const res = await axios.get(`https://api.tvmaze.com/shows/${showId}/episodes`);
 
 	// reduce results of API request to an array of objects containing episode data and pass array to populateEpisodes()
 	populateEpisodes(
 		res.data.reduce(function(episodesArr, nextEpisode) {
 			let imageUrl;
-			nextEpisode.image === null
-				? (imageUrl = defaultImage)
-				: (imageUrl = nextEpisode.image.original);
+			nextEpisode.image === null ? (imageUrl = defaultImage) : (imageUrl = nextEpisode.image.original);
 
 			episodesArr.push({
 				id          : nextEpisode.id,
 				name        : nextEpisode.name,
+				show        : showName,
 				summary     : nextEpisode.summary,
 				season      : nextEpisode.season,
 				number      : nextEpisode.number,
@@ -112,12 +109,15 @@ function populateEpisodes(episodes) {
 	// $episodesList.empty();
 
 	// modal
-	const $episodesModalList = $('#episodesModal-list');
+	const $episodesModalList = $('#episodesModalList');
 	$episodesModalList.empty();
+
+	let $episodesModalTitle = $('#episodesModalTitle');
+	$episodesModalTitle.text(episodes[0].show)
 
 	// create a new li for each show object in array
 	for (let episode of episodes) {
-		let itemHtml =`<b class="display-5">${episode.name}</b><br>`;
+		let itemHtml = `<b class="display-5">${episode.name}</b><br>`;
 
 		// build li content based on what information is available in episode object
 		if (episode.season !== null || episode.number !== null) itemHtml += '<p class="fst-italic">';
@@ -125,11 +125,11 @@ function populateEpisodes(episodes) {
 		if (episode.season !== null && episode.number !== null) itemHtml += ', ';
 		if (episode.number !== null) itemHtml += `episode ${episode.number}`;
 		if (episode.season !== null || episode.number !== null) itemHtml += '</p>';
-		if (episode.image !== defaultImage) itemHtml+= `<img src="${episode.image}" 
-		class="img-thumbnail" alt="..."></img>`
+		if (episode.image !== defaultImage)
+			itemHtml += `<img src="${episode.image}" 
+		class="img-thumbnail" alt="..."></img>`;
 		if (episode.summary !== null) itemHtml += `${episode.summary}`;
-		
-		
+
 		const $item = `<li class="episode">${itemHtml}</li>`;
 
 		// append new li to episodes list
@@ -146,10 +146,18 @@ function populateEpisodes(episodes) {
 
 /* ADD EVENT LISTENERS TO BUTTONS
  * hide episodes area, get list of matching shows and show in shows list.     */
-$('#search-form').on('submit', async function handleSearch(evt) {
+$('#search-form').on('submit', function(evt) {
 	evt.preventDefault();
+	searchInput('search-query');
+});
 
-	let query = $('#search-query').val();
+$('#navbarForm').on('submit', function(evt) {
+	evt.preventDefault();
+	searchInput('navbarSearchQuery');
+});
+
+async function searchInput(inputId) {
+	let query = $(`#${inputId}`).val();
 	if (!query) return;
 
 	$('#episodes-area').hide();
@@ -160,12 +168,13 @@ $('#search-form').on('submit', async function handleSearch(evt) {
 	// adds event listener to all buttons created at the bottom of show cards
 	$('.episodebutton').click(function(evt) {
 		const showId = evt.target.parentElement.getAttribute('data-show-id');
-		getEpisodes(showId);
+		const showName = evt.target.parentElement.getAttribute('data-show-name');
+		getEpisodes(showId, showName);
 
 		// show episodes modal
 		$('#episodesModal').modal('show');
 	});
-});
+}
 
 /* RANDOM STARTING SHOW IN SEARCH
  * enter a different show on reload.     */
@@ -198,3 +207,4 @@ function getRandomShow(shows) {
 	return shows[randomNum];
 }
 $('#search-query').val(getRandomShow(randomShows));
+$('#navbarSearchQuery').val(getRandomShow(randomShows));
